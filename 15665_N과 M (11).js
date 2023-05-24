a2569f88ff70c7532b73dd8af8924f3a09afe80b
bin000527 <bin000527@sookmyung.ac.kr>
2023-05-24 12:55:51 +0900

    15665_N과 M (11)

---
+++ 15665_N과 M (11).js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [[N, M], input] = fs
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split('\n')
+    .map((str) => str.split(' ').map((val) => +val));
+
+/**
+ * 조건
+ * 1. N개의 자연수 중에서 M개를 고른 수열
+ * 2. 같은 수를 여러 번 골라도 된다.
+ */
+
+// 15656번 문제와 비슷하지만 아래와 같은 차이점이 있다.
+
+// 주어진 N개의 자연수 중에서 같은 수가 존재하므로 중복된 수열이 존재할 수 있다.
+// ex: 3개의 자연 수, 4 4 2가 주어졌을 때 수열 "2 4"가 만들어지는 경우는 2가지([3번, 1번], [3번, 2번]) 이다.
+// 따라서 Set을 이용하여 중복된 값을 제거한다.
+const solution = () => {
+    let answer = [];
+    let tmp = Array.from({ length: M }, () => 0);
+
+    input.sort((a, b) => a - b);
+
+    const DFS = (L) => {
+        if (L === M) {
+            answer.push(tmp.join(' '));
+            return;
+        }
+
+        for (let i = 0; i < N; i++) {
+            tmp[L] = input[i];
+            DFS(L + 1);
+        }
+    };
+
+    DFS(0);
+    return [...new Set(answer)].join('\n');
+};
+
+console.log(solution());
